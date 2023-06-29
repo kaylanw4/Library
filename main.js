faeb04dd741024aa36daf5cb76103b669b53c87e
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-29 16:44:36 -0400

    implemented toggle read button for table elements

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// form modal open and close
 const modal = document.querySelector('#modal')
 const openModal = document.querySelector('.open-form')
 const closeModal = document.querySelector('.close-form')
@@ -6,37 +7,55 @@ openModal.addEventListener('click', () => {
     modal.showModal()
 })
 
-
 closeModal.addEventListener('click', () => {
     modal.close()
 })
 
-
+// create array to hold book objects
 let myLibrary = [];
-const form = document.querySelector('form')
 
+// create book objects from form submission
+const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const fd = new FormData(form)
     const obj = Object.fromEntries(fd)
     myLibrary.push(obj)
-    appendTable(myLibrary)
+    populateTable(myLibrary)
     form.reset()
     modal.close()
 })
 
-function appendTable(data) {
+/**
+ * updates the table with all of the book objects
+ * @param {array} library array of book objects
+ */
+function populateTable(library) {
     let tableBody = document.querySelector('#table-body')
     while (tableBody.firstChild) {
         tableBody.removeChild(tableBody.firstChild)
     }
-    data.forEach(object => {
+    library.forEach(object => {
         let fr = document.createElement('tr')
-        fr.innerHTML = '<td>' + object.title +
-        '<td>' + object.author +
-        '<td>' + object.pages + 
-        '<td>' + object.read;
+        fr.innerHTML = `<td>${object.title}` +
+        `<td>${object.author}` +
+        `<td>${object.pages}` +
+        `<td>${object.read}` +
+        `<td><button class='button toggle' data-index='${library.indexOf(object)}'>toggle`;
         tableBody.appendChild(fr)
+        let toggleBtn = document.querySelectorAll('.toggle')
+        toggleBtn.forEach(btn => {
+            btn.addEventListener('click', toggleRead)
+        })
     })
-        
+}
+
+function toggleRead(e){
+    let index = e.target.dataset.index
+    if (myLibrary[index].read === 'yes') {
+        myLibrary[index].read = 'no'
+    } else {
+        myLibrary[index].read = 'yes'
+    }
+    populateTable(myLibrary)
 }
